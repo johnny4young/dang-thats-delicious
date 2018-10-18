086a07f3e88d17ecb167c6eff04636e2bd002796
Johnny IV Young Ospino <asesordeprogramacion@gmail.com>
2018-10-18 15:52:40 -0500

    creating and editing flow for stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -12,7 +12,6 @@ exports.addStore = (req, res) => {
 exports.createStore = async(req, res) => {
 
 		const store = await (new Store(req.body)).save()
-		console.log('it works')
 		req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
 		res.redirect(`/store/${store.slug}`)
 }
@@ -20,6 +19,28 @@ exports.createStore = async(req, res) => {
 exports.getStores = async(req, res) => {
 	//1. query the database for a list of all stores
 	const stores = await Store.find()
-	console.log(stores)
 	res.render('stores', { title: 'Stores', stores})
+}
+
+exports.editStore = async(req, res) => {
+	//1. find the store given the ID
+	const store = await Store.findOne({ _id : req.params.id })
+	//2. confirm they are the owner of the store
+	//TODO
+	//3. render out the edit form so the user can update their store
+		res.render('editStore', {
+			title: `Edit ${store.name}`,
+			store
+		})
+}
+
+exports.updateStore= async(req, res) => {
+	//find and update the store
+	const  store = await Store.findByIdAndUpdate({ _id : req.params.id }, req.body, {
+		new: true, // return the new store instead of the old one
+		runValidators: true
+		}).exec()
+	req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store =></a>`)
+	//redirect them the store and tell them it worked
+	res.redirect(`/stores/${store._id}/edit`)
 }
