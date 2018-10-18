66936fdbd6a64db0f0b994739866044e8efe82c0
Johnny IV Young Ospino <asesordeprogramacion@gmail.com>
2018-10-18 15:18:04 -0500

    querying our database for stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
 
-exports.homePage = (req, res) => {
-	console.log(req.name)
+exports.homePage = async(req, res) => {
 	res.render('index')
 }
 
@@ -12,8 +11,15 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async(req, res) => {
 
-		const store = await (new Store(req.body)).save();
+		const store = await (new Store(req.body)).save()
 		console.log('it works')
 		req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
 		res.redirect(`/store/${store.slug}`)
+}
+
+exports.getStores = async(req, res) => {
+	//1. query the database for a list of all stores
+	const stores = await Store.find()
+	console.log(stores)
+	res.render('stores', { title: 'Stores', stores})
 }
