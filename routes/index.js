cb14045a00be7067d3a19bda4f8421efa0fae40f
Johnny IV Young Ospino <asesordeprogramacion@gmail.com>
2019-01-02 10:46:44 -0500

    Saving Registered Users to the Database

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const storeController = require('../controllers/storeController')
 const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 const {catchErrors} = require('../handlers/errorHandlers')
 // Do work here
 router.get('/', catchErrors(storeController.getStores))
@@ -28,6 +29,9 @@ router.get('/register', userController.registerForm)
 //1. validate the registration data
 //2. register the user
 //3. we need to log them in
-router.post('/register', userController.validateRegister)
+router.post('/register',
+userController.validateRegister,
+userController.register,
+authController.login)
 
 module.exports = router;
