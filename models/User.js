e61d22355f1f8e27104fda9f3eec2f83acc24fa4
Johnny IV Young Ospino <asesordeprogramacion@gmail.com>
2019-01-03 16:06:40 -0500

    virtual fields login-logout protecting routes

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 mongoose.Promise = global.Promise
-
 const md5 = require('md5')
 const validator = require('validator')
 const mongodbErrorHandler = require('mongoose-mongodb-errors')
@@ -21,6 +20,11 @@ const userSchema = new Schema({
 		required: 'Please supply a name',
 		trim: true
 	}
+})
+
+userSchema.virtual('gravatar').get(function(){
+	const hash = md5(this.email)
+	return `https://gravatar.com/avatar/${hash}?s=200`
 })
 
 userSchema.plugin(passportLocalMongoose, { usernameField: 'email'})
