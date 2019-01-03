225cce8b388bdf2ae343fbba96c0e9731345631d
Johnny IV Young Ospino <asesordeprogramacion@gmail.com>
2019-01-03 16:36:27 -0500

    creating user, edit

--- routes/index.js
+++ routes/index.js
@@ -37,4 +37,7 @@ authController.login)
 
 router.get('/logout', authController.logout)
 
+router.get('/account', authController.isLoggedIn,  userController.account)
+router.post('/account', catchErrors(userController.updateAccount))
+
 module.exports = router;
